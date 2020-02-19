41fcc03ee9056863433a76b0f61a3dad715264b0
Anthony Castillo <ACastillo@ipsoft.com>
2020-02-18 21:17:48 -0500

    Updated Mobile
    Messed up medium website, but imrpved small massively

--- src/components/HomeLanding.js
+++ src/components/HomeLanding.js
@@ -1,16 +1,15 @@
 import React from "react";
 import candleShop from "../images/candle1.jpg"
 import candleNew from "../images/candle3.jpg"
-import cpu from "../images/computer.jpg";
 
 const HomeLanding = () => {
     return (
         <div className={"homelanding-container"}>
+
             <div className={"homelanding-banner"}>
                 Exclusive 20% sale happening now! Discount applied at check out.
             </div>
 
-            
             <div className={"homelanding-textinfo"}>
                 <h1>Natural Soy Candles</h1>
                 <p>Handcrafted & Homemade</p>
@@ -18,12 +17,22 @@ const HomeLanding = () => {
             </div>
 
             <div className={"homelanding-image"}>
-                <img src={cpu} /> 
-
-                <div className={"homelanding-small"}>
-                    Inventory
+                <div className={"homelanding-text"}>
+                    <span>
+                        Welcome to Vibras.
+                    </span>
+                    <span>
+                        The Candles That You Will Forever Love.
+                    </span>
+                    <span>
+                        Handmade all natural candles
+                    </span>
+                    
+                    <div className={"homelanding-text-buttons"}>
+                        <button>Shop Vibras Handmade Candles</button>
+                        <button>Learn more about Vibras.</button>
+                    </div>
                 </div>
-            
             </div>
         </div>
     )
--- src/components/Navi.js
+++ src/components/Navi.js
@@ -2,9 +2,37 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+
+
+const openMobileNavi = (event) => {
+    event.preventDefault();
+    const naviOverlayWidth = document.getElementsByClassName("navi-overlay")[0];
+    naviOverlayWidth.style.width = "100%";
+};
+
+const closeMobileNavi = (event) => {
+    event.preventDefault();
+    const naviOverlayWidth = document.getElementsByClassName("navi-overlay")[0];
+    naviOverlayWidth.style.width = "0%";
+};
+
 const Navi = () => {
     return (
         <div className={"navi-container"}>
+
+            <div className={"navi-overlay"} onClick={closeMobileNavi}>
+                <div className={"navi-overlay-close"}>
+                    <span>Click to close</span>
+                </div>
+
+                <div className={"navi-overlay-content"}>
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Clients</a>
+                    <a href="#">Contact</a>
+                </div>
+            </div>
+
             <div className={"navi-brand"}>
                 <Link to={"/"}>
                         <span>Vibras</span>
@@ -21,6 +49,18 @@ const Navi = () => {
                 <div><Link to={"/"}>About</Link></div>
                 <div><Link to={"/shop"}>Shop</Link></div>
                 <div><Link to={"/"}>Cart</Link></div>
+            </div>
+
+            <div className={"navi-options-sm"}>
+                <div className={"navi-options-sm-left"} onClick={openMobileNavi}>
+                    <span className="material-icons">reorder</span>
+                </div>
+                <div className={"navi-options-sm-right"}>
+                    <span className="material-icons">search</span>
+                    <span className="material-icons">shopping_cart</span>
+                </div>
+                
+
             </div>
         </div>
     )
