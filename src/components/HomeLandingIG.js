056e4228b36680fccf069d0e09c2d327addd873e
Anthony Castillo <ACastillo@ipsoft.com>
2020-02-27 21:25:12 -0500

    Updated button
    Button update

--- src/components/HomeLandingIG.js
+++ src/components/HomeLandingIG.js
@@ -4,13 +4,15 @@ import candle1 from "../images/candle1.jpg";
 import candle2 from "../images/candle2.jpg";
 import candle3 from "../images/candle3.jpg";
 import candle4 from "../images/candle4.jpg";
+import candle5 from "../images/candle5.jpg";
+import candle6 from "../images/candle6.jpg";
 
 const HomeLandingIG = () => {
     return (
         <div className={"homelanding-IG-container"}>
             
-            <h2>Instagram</h2>
-
+            <h2>Vibras On Instagram</h2>
+            <p>Loren Ipsum</p>
             <div className={"homelanding-IG-images"}>
                 <div>
                     <img src={candle1} />
@@ -25,12 +27,14 @@ const HomeLandingIG = () => {
                     <img src={candle4} />
                 </div>
                 <div>
-                    <img src={candle1} />
+                    <img src={candle5} />
                 </div>
                 <div>
-                    <img src={candle2} />
+                    <img src={candle6} />
                 </div>               
             </div>
+            
+            <button>Instagram</button>
         </div>
     )
 };
